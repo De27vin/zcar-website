6b9871bff22447fde8afbead7c2c1f4f3244679f
Devin de Spindler <devin.vipxxx@gmail.com>
2024-08-30 14:24:52 +0200

    feat: Home, Kontakt, Angebote and Ueber -.js pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
+import Angebote from "./pages/Angebote.js";
+import Kontakt from "./pages/Kontakt.js";
+import Ueber from "./pages/Ueber.js";
 
 export default function App() {
 
