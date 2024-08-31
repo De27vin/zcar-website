7101c8acf26b49ec779d387f3b3149a1f7303a0e
mikeezvz <mike.zogheib@outlook.com>
2024-09-01 01:37:06 +0200

    feat: basic header component
    The header component is set up and the basic stuff like navbar, a logo and theme switcher were added. The header doesn't have to be finished already though. Changes can still be made in the future.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { ThemeProvider } from './context/ThemeContext';
+import Header from "./components/Header"
 import Home from "./pages/Home";
 import Angebote from "./pages/Angebote";
 import Kontakt from "./pages/Kontakt";
@@ -7,37 +9,12 @@ import Ueber from "./pages/Ueber";
 
 export default function App() {
   return (
-    <Router>
-      <header>
-        <nav className="bg-white shadow dark:bg-gray-800">
-          <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-            <NavButton path="/" label="Home" />
-            <NavButton path="/angebote" label="Angebote" />
-            <NavButton path="/kontakt" label="Kontakt" />
-            <NavButton path="/ueber-uns" label="Über Uns" />
-          </div>
-        </nav>
-      </header>
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/angebote" element={<Angebote />} />
-        <Route path="/kontakt" element={<Kontakt />} />
-        <Route path="/ueber-uns" element={<Ueber />} />
-      </Routes>
-    </Router>
+    <ThemeProvider>
+    <body>
+      <Header/>
+    </body>
+    </ThemeProvider>
   );
 }
 
-function NavButton({ path, label }) {
-  const navigate = useNavigate();
 
-  return (
-    <button
-      onClick={() => navigate(path)}
-      className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-    >
-      {label}
-    </button>
-  );
-}
---
+++ src/components/Header.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Sun, Moon } from 'lucide-react';
+import { useTheme } from '../context/ThemeContext';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import Home from "../pages/Home";
+import Angebote from "../pages/Angebote";
+import Kontakt from "../pages/Kontakt";
+import Ueber from "../pages/Ueber";
+
+const Header = () => {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <header>
+        <Router>
+        <div class="grid grid-cols-3 gap-4 m-10">
+            <div className='Logo'>
+                <img src='image/car.jpg' class="h-28"></img>
+            </div>
+            <nav className="bg-white shadow dark:bg-gray-800 rounded-lg flex items-center">
+            <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+                <NavButton path="/" label="Home" />
+                <NavButton path="/angebote" label="Angebote" />
+                <NavButton path="/kontakt" label="Kontakt" />
+                <NavButton path="/ueber-uns" label="Über Uns" />
+            </div>
+            </nav>
+            <div className='flex justify-end'>
+            <button
+                onClick={toggleTheme}
+                className="p-1 rounded-2xl bg-gray-300 dark:bg-gray-700 flex items-center justify-center w-16 h-16"
+            >
+                {theme === 'light' ? (
+                    <Sun className="text-yellow-500 h-4 w-4" />
+                ) : (
+                    <Moon className="text-white h-4 w-4" />
+                )}
+            </button>
+            </div>
+        </div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/angebote" element={<Angebote />} />
+            <Route path="/kontakt" element={<Kontakt />} />
+            <Route path="/ueber-uns" element={<Ueber />} />
+        </Routes>
+      </Router>
+      </header>
+  );
+};
+function NavButton({ path, label }) {
+    const navigate = useNavigate();
+  
+    return (
+      <button
+        onClick={() => navigate(path)}
+        className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+      >
+        {label}
+      </button>
+    );
+  }
+
+export default Header;
