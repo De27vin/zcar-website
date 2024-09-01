cf181271acc098be6fc243b7d03ca420adf93673
mikeezvz <mike.zogheib@outlook.com>
2024-09-01 14:12:45 +0200

    feat: location video added
    A video for the company's location was added to the homepage.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,19 @@ export default function App() {
 
 
     return (
+      <>
+      <div class="grid grid-cols-3 m-15">
+        <div/>
+        <h1 class="flex justify-center" >Unser Standort</h1>
+      </div>
+      <div class="grid grid-cols-3 m-10">
+        <div></div>
         <div>
-    
-    
-    
+          <video class="flex justify-center" controls>
+            <source src="video/car.mp4" type="video/mp4"/>
+          </video>  
         </div>
+        </div>
+        </>
       );
     }
