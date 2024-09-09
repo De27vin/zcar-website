743539957cf0d37c83f7b47b14d54e2c96d52be9
Devin de Spindler <devin.vipxxx@gmail.com>
2024-09-09 12:39:20 +0200

    feat: Add footer with redirecting logo, autoscout24 icon and github repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { ThemeProvider } from './context/ThemeContext';
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
     <ThemeProvider>
     <div className="bg-gray-100 dark:bg-gray-600 min-h-screen pt-10">
       <Header/>
+      <Footer/>
     </div>
     </ThemeProvider>
   );
