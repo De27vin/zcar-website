8231344cab00d82e1cdb6a4b114dcf1a67267e7d
mikeezvz <mike.zogheib@outlook.com>
2024-09-13 13:30:45 +0200

    fix: homepage restructured
    The homepage was restructured and the location video and the embedded Google Maps location were put in the same grid row. Also the reviews are now being flexed with flex-row and not flex-col.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,36 +9,29 @@ export default function App() {
     return (
       <>
       {/* 1. Column with grid */}
-      <div class="grid grid-cols-3 m-15">
-        <div></div>
+      <div class="grid grid-cols-2 m-20 place-items-center">
 
-        <div className="video-home-page">
-          <video autoPlay muted loop class="flex justify-center rounded-lg" controls>
+        <div className="video-home-page" class="h-full w-full">
+          <video autoPlay muted loop class="flex justify-center rounded-lg ring-4 ring-black" controls>
             <source src="/video/video-bmw-m4.mp4" type="video/mp4"/>
           </video>  
         </div>
-        </div>
-
-        {/* 2. Column with grid */}
-        <div class="grid grid-cols-3 m-15">
-          <div/>
-          <h1 class="flex justify-center" >Unser Standort</h1>
-        </div>
 
         {/* 3. Column with grid */}
-        <div class="grid grid-cols-3 m-15">
-          <div></div>
 
-          <div class="flex justify-center">
+          <div class="flex justify-center h-full w-full">
             <iframe class="rounded-lg" className="location-google-maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1633.5759552906436!2d8.596094360471284!3d47.45025472083464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479a9f8de213ef9d%3A0x1957b6eda0ad69f!2sBassersdorferstrasse%20111%2C%208302%20Kloten!5e1!3m2!1sde!2sch!4v1725301551324!5m2!1sde!2sch" title="Location of Z-Car on Google Maps" width="640" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
 
-        <div class="grid grid-cols-3 m-15">
+        <hr class="w-4/6 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+
+        <div class="grid grid-cols-1 m-15">
           <div></div>
-        <div class="h-12 rounded-2xl shadow-2xl w-full h-lvh p-10">
-        <div class="flex flex-col items-center">
+        <div class="h-12 rounded-2xl shadow-2xl w-full h-96 p-10">
         <div><h1 class="font-bold text-5xl dark:text-white mb-10">Was unsere Kunden sagen</h1></div>
+        <div class="flex flex-row items-center">
+
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
   <img class="w-full" src="/img/card-top.jpg" alt=""/>
   <div class="px-6 py-4 w-96">
