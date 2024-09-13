aa93e84afb6c98498a5b00220b7c26c3f48a7c3a
mikeezvz <mike.zogheib@outlook.com>
2024-09-13 12:08:36 +0200

    add: Impressum
    An Impressum was added

--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -7,6 +7,7 @@ export default function App() {
 
  
     return (      
+      <>
  	<div class=" my-4 px-4 lg:px-20 flex-items-center">
     <div class="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
       <div class="flex">
@@ -26,15 +27,25 @@ export default function App() {
           <textarea placeholder="Message*" class="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
         </div>
         <div class="my-2 w-1/2 lg:w-1/4">
-          <button class="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 w-full 
+          <button class="uppercase text-sm font-bold tracking-wide bg-blue-800 hover:bg-blue-900 text-gray-100 p-3 w-full 
                       focus:outline-none focus:shadow-outline rounded-lg">
             Senden
           </button>
         </div>
   </div>
-
-    
-    
         </div>
+        <div class="grid grid-cols-3">
+        <div class="h-12 rounded-2xl shadow-2xl w-full h-96 p-10 ml-20 mt-20">
+          <h1 class="font-bold text-4xl dark:text-white">Impressum</h1>
+          <br/>  
+            <p class="leading-loose text-xl text-gray-700 text-base dark:text-slate-300">
+            Herr Hussein Zogheib<br/>
+            Z-Car Automobile - Auto Ankauf & Verkauf<br/>
+            Bassersdorferstrasse 117<br/>
+            8302 Kloten
+            </p>
+        </div>
+        </div>
+        </>
       );
     }
